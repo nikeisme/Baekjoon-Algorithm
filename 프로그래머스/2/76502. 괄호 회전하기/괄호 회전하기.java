51950280bd0296b9bc0589b8905e0de6f51dde08
yu jenog, Hwang <99165573+nikeisme@users.noreply.github.com>
2025-05-17 20:40:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 96.26 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,31 +1,30 @@
-import java.util.Stack;
+import java.util.*;
 
-public class Solution {
-    static public int solution(String s) {
-        int answer = 0;
+class Solution {
 
-        for (int i = 0; i < s.length(); i++) {
-            Stack<Character> stack = new Stack<>();
-            String str = s.substring(i, s.length()) + s.substring(0, i);
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (stack.isEmpty()) {
-                    stack.push(c);
-                } else if (c == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
-            }
-            if (stack.isEmpty()) {
-                answer++;
-            }
-        }
+    // 조합을 담을 배열
+    List<String> list = new ArrayList<>();
 
+    String[] correct = {"(), [], {}"};
+    
+    public int solution(String s) {
+        int answer = -1;
+        
+        // 모든 조합해보는 함수
+        dfs(s);
         return answer;
+    }
+    
+    private void dfs(String current){
+        
+        // 길이가 1000 이상이면 반환
+        if (current.length() > 1000) return;
+        
+        // 배열에 문자열 담기 
+        if(!current.isEmpty()) list.add(current);
+        
+        System.out.println(list.toString());
+        
+
     }
 }
